78ec29d65b9ffdf5d8e9edb7f9ceaccc2a4759e7
Quinn Mosher <quinn.jmosher@gmail.com>
2016-02-18 10:20:25 -0800

    scrolling background + some prefab fixes

---
+++ TasteTheRainbow/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public GameObject background0;
+    public GameObject background1;
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    public float scrollSpeed = -1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        background0.transform.Translate(0, scrollSpeed * Time.deltaTime, 0);
+        if (background0.transform.position.y <= endPoint.transform.position.y)
+        {
+            background0.transform.position = new Vector3(0, startPoint.transform.position.y, 1);
+        }
+
+        background1.transform.Translate(0, scrollSpeed * Time.deltaTime, 0);
+        if (background1.transform.position.y <= endPoint.transform.position.y)
+        {
+            background1.transform.position = new Vector3(0, startPoint.transform.position.y, 1);
+        }
+    }
+}
