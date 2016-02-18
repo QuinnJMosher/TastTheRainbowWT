a8f89015614ecb0fac1f6a70e92a201ced78e6f2
Adrian Caple <adrianocaple@gmail.com>
2016-02-18 10:33:44 -0800

    weapon ui working

--- TasteTheRainbow/Assets/ChangeWeaponSprite.cs
+++ TasteTheRainbow/Assets/ChangeWeaponSprite.cs
@@ -4,42 +4,50 @@ using UnityEngine.UI;
 
 public class ChangeWeaponSprite : MonoBehaviour
 {
-    public Sprite blueWeapon;
-    public Sprite yellowWeapon;
-    public Sprite redWeapon;
-    public Sprite orangeWeapon;
-    public Sprite purpleWeapon;
-    public Sprite greenWeapon;
-    public Image weaponUI;
+    public Image blueWeapon;
+    public Image yellowWeapon;
+    public Image redWeapon;
+    public Image orangeWeapon;
+    public Image purpleWeapon;
+    public Image greenWeapon;
+    private Image selectedWeapon;
 
     // Use this for initialization
     void Start()
     {
+        selectedWeapon = blueWeapon;
     }
 
     // Update is called once per frame
     void Update()
     {
+        selectedWeapon.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 27);
+        selectedWeapon.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 35);
         switch (PlayerColor.playerColor)
         {
             case ColorDefs.DefiniteColor.CO_BLUE:
-                weaponUI.sprite = blueWeapon;
+                Debug.Log("Blue Weapon!");
+                selectedWeapon = blueWeapon;
+                //selectedWeapon.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 35.0f);
+                //selectedWeapon.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 44.0f);
                 break;
             case ColorDefs.DefiniteColor.CO_YELLOW:
-                weaponUI.sprite = yellowWeapon;
+                selectedWeapon = yellowWeapon;
                 break;
             case ColorDefs.DefiniteColor.CO_RED:
-                weaponUI.sprite = redWeapon;
+                selectedWeapon = redWeapon;
                 break;
             case ColorDefs.DefiniteColor.CO_ORANGE:
-                weaponUI.sprite = orangeWeapon;
+                selectedWeapon = orangeWeapon;
                 break;
             case ColorDefs.DefiniteColor.CO_PURPLE:
-                weaponUI.sprite = purpleWeapon;
+                selectedWeapon = purpleWeapon;
                 break;
             case ColorDefs.DefiniteColor.CO_GREEN:
-                weaponUI.sprite = greenWeapon;
+                selectedWeapon = greenWeapon;
                 break;
         }
+        selectedWeapon.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 35.0f);
+        selectedWeapon.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 44.0f);
     }
 }
